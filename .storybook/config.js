4e3f4fc346b9f6d6066746aabfb667f4189aecdb
Daniel Thor Gunnlaugsson <daniel.gunnlaugsson@bouvet.no>
2018-09-24 15:03:30 +0200

    Chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react';
 import '../src/index.css';
+import 'storybook-chromatic/storybook-addon';
 
-const req = require.context('../src', true, /.stories.js$/);
+const req = require.context('../src/components', true, /\.stories\.js$/);
+
+// Chromatic: ./node_modules/.bin/chromatic test --storybook-addon --app-code="" --do-not-start
 
 function loadStories() {
   //require('../src/stories');
