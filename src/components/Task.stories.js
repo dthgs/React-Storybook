c8bc8de8c6fdadaa81cce134c415e6ef697d2cd7
Daniel Thor Gunnlaugsson <daniel.gunnlaugsson@bouvet.no>
2018-09-24 16:01:35 +0200

    build-storybook script and addons

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
 
 // https://www.learnstorybook.com/react/en
 // npm run storybook
 // Storyshots addon: npm add --dev @storybook/addon-storyshots react-test-renderer
+// npm add react-redux
 
 export const task = {
   id: '1',
@@ -14,6 +16,8 @@ export const task = {
   state: 'TASK_INBOX',
   updatedAt: new Date(2018, 0, 1, 9, 0),
 };
+
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
 
 // Actions help you verify interactions when building UI components in isolation. 
 // Oftentimes you won't have access to the functions and state you have in context of the app. Use action() to stub them in. 
@@ -27,6 +31,10 @@ export const actions = {
 // action() creates a callback that appears in actions panel
 
 storiesOf('Task', module)
-  .add('default', () => <Task task={task} {...actions} />)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    return <Task task={object('task', { ...task })} {...actions} />
+  })
   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />)
+  .add('long title', () => <Task task={{ ...task, title: longTitle }} {...actions} />);;
